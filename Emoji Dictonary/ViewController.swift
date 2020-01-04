2a365c5ead982ba1baee8a889b9a834872ae97a0
Nicholas <Nick_3721@hotmail.com>
2020-01-04 19:23:48 +0000

    Initial Commit

---
+++ Emoji Dictonary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictonary
+//
+//  Created by Nicholas MacDonald on 1/4/20.
+//  Copyright © 2020 Nicholas MacDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
